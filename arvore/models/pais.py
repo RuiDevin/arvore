cb2d6943845f057d61089c9f443f78bb68b9d08d
ruidevin <rafaelalvaro00@gmail.com>
2023-11-04 14:17:12 -0300

    adião many to many

--- arvore/models/pais.py
+++ arvore/models/pais.py
@@ -1,10 +1,12 @@
 from django.db import models
 
+from .irmaos import Irmaos 
 
 class Pais(models.Model):
     nome_completo = models.CharField(max_length=255)
     data_de_nascimento = models.DateField()
     cidade_natal = models.CharField(max_length=255)
+    filhos = models.ManyToManyField(Irmaos, related_name='pais')
 
     def __str__(self):
         return self.nome_completo
