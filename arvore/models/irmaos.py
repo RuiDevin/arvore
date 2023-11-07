fe81bf8a03ba42b3d10a63b48153337872af5846
ruidevin <rafaelalvaro00@gmail.com>
2023-11-07 14:31:11 -0300

    correção back-end

--- arvore/models/irmaos.py
+++ arvore/models/irmaos.py
@@ -1,12 +1,15 @@
 from django.db import models
 
 from .escolaridade import Escolaridade
+from .endereco import Endereco
+from .trabalho import Trabalho
 
 class Irmaos(models.Model):
     nome_completo = models.CharField(max_length=255)
     data_de_nascimento = models.DateField()
-    cidade_natal = models.CharField(max_length=255)
+    endereco = models.ManyToManyField(Endereco,related_name='endereco_irmao')
     escolaridade = models.ForeignKey(Escolaridade, on_delete=models.PROTECT, related_name='escolaridade')
+    trabalho = models.ManyToManyField(Trabalho,related_name='suas_profissoes')
     pai = models.CharField(max_length=255)
     mae = models.CharField(max_length=255)
 
--- arvore/models/trabalho.py
+++ arvore/models/trabalho.py
@@ -1,7 +1,8 @@
 from django.db import models
 
 from .endereco import Endereco
+from .profissão import Profissao
 
 class Trabalho(models.Model):
     endereço = models.ManyToManyField(Endereco,related_name='endereco')
-    
+    profissao = models.ManyToManyField(Profissao, related_name='sua_profissao' )
